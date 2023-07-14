00ed1375060810dd5126b8bbd94e6e356bdadbf8
Decent Boy <88884010+decentboyy@users.noreply.github.com>
2023-07-14 10:23:07 +0530

    Update config.py
    Signed-off-by: Decent Boy <88884010+decentboyy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Octave_support")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/pglpnti_ki_duniya")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Iink_ka_adda")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
